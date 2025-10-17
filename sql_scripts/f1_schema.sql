7c97614069f5f85b40521d1a6cda446ed08a2f26
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-10-18 00:12:42 +0200

    feat: Design and create initial database schema for F1 project

---
+++ sql_scripts/f1_schema.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS winners;
+DROP TABLE IF EXISTS races;
+DROP TABLE IF EXISTS teams;
+DROP TABLE IF EXISTS drivers;
+
+CREATE TABLE drivers (
+                         id SERIAL PRIMARY KEY,
+                         name VARCHAR(128) NOT NULL UNIQUE,
+                         nationality VARCHAR(128)
+);
+
+CREATE TABLE teams (
+                       id SERIAL PRIMARY KEY,
+                       name VARCHAR(128) NOT NULL UNIQUE,
+                       nationality VARCHAR(128)
+);
+
+CREATE TABLE races (
+                       id SERIAL PRIMARY KEY,
+                       name VARCHAR(128) NOT NULL,
+                       circuit_name VARCHAR(128),
+                       continent VARCHAR(128),
+                       race_date DATE
+);
+
+CREATE TABLE winners (
+                         id SERIAL PRIMARY KEY,
+                         driver_id INT REFERENCES drivers(id) ON DELETE CASCADE,
+                         race_id INT REFERENCES races(id) ON DELETE CASCADE,
+                         team_id INT REFERENCES teams(id) ON DELETE CASCADE
+);
